3d9c0b7d9234dff7c67aa854e9a8566b5dbca2d3
Janne Salenius <janne.salenius@arbetsformedligen.se>
2017-09-04 16:20:38 +0200

    First commit

---
+++ BlockSizeCalculator/BlockSizeCalculator/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BlockSizeCalculator
+//
+//  Created by Janne Salenius on 2017-09-04.
+//  Copyright © 2017 Janne Salenius. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
